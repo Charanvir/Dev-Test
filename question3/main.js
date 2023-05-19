ba8d9049bfd1f764602bcacb4257ad0776f2e6d5
Charanvir <charanvir123@gmail.com>
2023-05-19 02:21:20 -0400

    Implemented multi directional linked list to Connect4

--- question3/main.js
+++ question3/main.js
@@ -1,107 +1,162 @@
 "use strict";
 exports.__esModule = true;
 exports.Connect4 = void 0;
-/**
- * Connect4 Class
- * Represents a Connect 4 game board and provides methods to play the game.
- */
+var GridNode = /** @class */ (function () {
+    function GridNode(value) {
+        this.value = value;
+        this.top = null;
+        this.bottom = null;
+        this.left = null;
+        this.right = null;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    return GridNode;
+}());
+var ROWS = 6;
+var COLUMNS = 7;
 var Connect4 = /** @class */ (function () {
-    /**
-     * Creates an instance of the Connect4 class.
-     * Initializes the game grid, current player, and winner.
-     */
     function Connect4() {
-        this.grid = [];
-        for (var i = 0; i < 6; i++) {
-            this.grid[i] = [];
-            for (var j = 0; j < 7; j++) {
-                this.grid[i][j] = 0;
+        this.currentPlayer = 1;
+        this.grid = this.createGrid();
+        this.gameFinished = false;
+    }
+    Connect4.prototype.createGrid = function () {
+        var ROWS = 6;
+        var COLUMNS = 7;
+        var grid = [];
+        // Initialize the first row separately
+        var firstRow = [];
+        for (var c = 0; c < COLUMNS; c++) {
+            firstRow.push(new GridNode(0));
+            if (c > 0) {
+                firstRow[c].left = firstRow[c - 1];
+                firstRow[c - 1].right = firstRow[c];
             }
         }
-        this.currentPlayer = 1;
-        this.winner = null;
-    }
-    /**
-     * Plays a move in the specified column.
-     * @param col - The column number (0-6) where the player wants to place their token.
-     * @returns A string indicating the result of the move.
-     */
-    Connect4.prototype.play = function (col) {
-        if (this.winner !== null) {
+        grid.push(firstRow);
+        // Initialize the rest of the grid
+        for (var r = 1; r < ROWS; r++) {
+            var row = [];
+            for (var c = 0; c < COLUMNS; c++) {
+                row.push(new GridNode(0));
+                if (c > 0) {
+                    row[c].left = row[c - 1];
+                    row[c - 1].right = row[c];
+                    row[c].topLeft = grid[r - 1][c - 1];
+                    grid[r - 1][c - 1].bottomRight = row[c];
+                }
+                if (c < COLUMNS - 1) {
+                    row[c].topRight = grid[r - 1][c + 1];
+                    grid[r - 1][c + 1].bottomLeft = row[c];
+                }
+                row[c].top = grid[r - 1][c];
+                grid[r - 1][c].bottom = row[c];
+            }
+            grid.push(row);
+        }
+        return grid;
+    };
+    Connect4.prototype.play = function (column) {
+        if (this.gameFinished) {
             return "Game has finished!";
         }
-        for (var row = 5; row >= 0; row--) {
-            if (this.grid[row][col] === 0) {
-                // Empty cell found, place the player's token
-                this.grid[row][col] = this.currentPlayer;
-                if (this.checkWin(row, col)) {
-                    // The current player wins the game
-                    this.winner = this.currentPlayer;
-                    return "Player ".concat(this.currentPlayer, " wins!");
-                }
-                else {
-                    // Switch to the next player's turn
-                    var message = "Player ".concat(this.currentPlayer, " has a turn");
-                    this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
-                    return message;
-                }
-            }
+        if (column < 0 || column >= this.grid[0].length) {
+            return "Invalid column!";
         }
-        // No empty cell found in the specified column
+        var currentRow = this.grid.length - 1;
+        while (currentRow >= 0) {
+            if (this.grid[currentRow][column].value === 0) {
+                this.grid[currentRow][column].value = this.currentPlayer;
+                if (this.checkWin(currentRow, column)) {
+                    this.gameFinished = true;
+                    return "Player " + this.currentPlayer + " wins!";
+                }
+                this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+                return "Player " + this.currentPlayer + " has a turn";
+            }
+            currentRow--;
+        }
         return "Column full!";
     };
-    /**
-     * Checks if the current player has won the game.
-     * @param row - The row number of the last placed token.
-     * @param col - The column number of the last placed token.
-     * @returns True if the current player has won, false otherwise.
-     */
-    Connect4.prototype.checkWin = function (row, col) {
-        // Check vertical
-        var count = 0;
-        for (var i = 0; i < 6; i++) {
-            count = this.grid[i][col] === this.currentPlayer ? count + 1 : 0;
-            if (count >= 4)
-                return true;
+    Connect4.prototype.checkWin = function (row, column) {
+        var player = this.grid[row][column].value;
+        // Horizontal check
+        var count = 1;
+        var i = column - 1;
+        while (i >= 0 && this.grid[row][i].value === player) {
+            count++;
+            i--;
         }
-        // Check horizontal
-        count = 0;
-        for (var i = 0; i < 7; i++) {
-            count = this.grid[row][i] === this.currentPlayer ? count + 1 : 0;
-            if (count >= 4)
-                return true;
+        i = column + 1;
+        while (i < this.grid[row].length && this.grid[row][i].value === player) {
+            count++;
+            i++;
+        }
+        if (count >= 4) {
+            return true;
+        }
+        // Vertical check
+        count = 1;
+        var j = row - 1;
+        while (j >= 0 && this.grid[j][column].value === player) {
+            count++;
+            j--;
+        }
+        j = row + 1;
+        while (j < this.grid.length && this.grid[j][column].value === player) {
+            count++;
+            j++;
+        }
+        if (count >= 4) {
+            return true;
+        }
+        // Diagonal check: top-left to bottom-right
+        count = 1;
+        i = column - 1;
+        j = row - 1;
+        while (i >= 0 && j >= 0 && this.grid[j][i].value === player) {
+            count++;
+            i--;
+            j--;
+        }
+        i = column + 1;
+        j = row + 1;
+        while (i < this.grid[row].length &&
+            j < this.grid.length &&
+            this.grid[j][i].value === player) {
+            count++;
+            i++;
+            j++;
+        }
+        if (count >= 4) {
+            return true;
+        }
+        // Diagonal check: top-right to bottom-left
+        count = 1;
+        i = column + 1;
+        j = row - 1;
+        while (i < this.grid[row].length &&
+            j >= 0 &&
+            this.grid[j][i].value === player) {
+            count++;
+            i++;
+            j--;
+        }
+        i = column - 1;
+        j = row + 1;
+        while (i >= 0 && j < this.grid.length && this.grid[j][i].value === player) {
+            count++;
+            i--;
+            j++;
+        }
+        if (count >= 4) {
+            return true;
         }
         return false;
     };
     return Connect4;
 }());
 exports.Connect4 = Connect4;
-// Testing a vertical win
-var gameExample = new Connect4();
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0)); // Expected Result is Player 1 wins
-// Testing when a column is full
-var gameExample2 = new Connect4();
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0)); // Expected Column Full
-console.log(gameExample2.play(0)); // Expected Column Full
-// Testing a horizontal win
-var gameExample3 = new Connect4();
-console.log(gameExample3.play(0));
-console.log(gameExample3.play(0));
-console.log(gameExample3.play(1));
-console.log(gameExample3.play(1));
-console.log(gameExample3.play(2));
-console.log(gameExample3.play(2));
-console.log(gameExample3.play(3)); // Expected Player 1 Wins
-console.log(gameExample3.play(3)); // Expected Game Finished
--- question3/main.ts
+++ question3/main.ts
@@ -1,114 +1,193 @@
-/**
- * Connect4 Class
- * Represents a Connect 4 game board and provides methods to play the game.
- */
-export class Connect4 {
-  private grid: number[][]; // The game grid represented as a 2D array of numbers
-  private currentPlayer: number; // The current player (1 or 2)
-  private winner: number | null; // The winner of the game (1 or 2) or null if there is no winner yet
+class GridNode {
+  value: number;
+  top: GridNode | null;
+  bottom: GridNode | null;
+  left: GridNode | null;
+  right: GridNode | null;
+  topLeft: GridNode | null;
+  topRight: GridNode | null;
+  bottomLeft: GridNode | null;
+  bottomRight: GridNode | null;
 
-  /**
-   * Creates an instance of the Connect4 class.
-   * Initializes the game grid, current player, and winner.
-   */
+  constructor(value: number) {
+    this.value = value;
+    this.top = null;
+    this.bottom = null;
+    this.left = null;
+    this.right = null;
+    this.topLeft = null;
+    this.topRight = null;
+    this.bottomLeft = null;
+    this.bottomRight = null;
+  }
+}
+
+const ROWS = 6;
+const COLUMNS = 7;
+
+export class Connect4 {
+  currentPlayer: number;
+  grid: GridNode[][];
+  gameFinished: boolean;
+
   constructor() {
-    this.grid = [];
-    for (let i = 0; i < 6; i++) {
-      this.grid[i] = [];
-      for (let j = 0; j < 7; j++) {
-        this.grid[i][j] = 0;
-      }
-    }
     this.currentPlayer = 1;
-    this.winner = null;
+    this.grid = this.createGrid();
+    this.gameFinished = false;
   }
 
-  /**
-   * Plays a move in the specified column.
-   * @param col - The column number (0-6) where the player wants to place their token.
-   * @returns A string indicating the result of the move.
-   */
-  play(col: number): string {
-    if (this.winner !== null) {
+  createGrid(): GridNode[][] {
+    const ROWS = 6;
+    const COLUMNS = 7;
+    let grid: GridNode[][] = [];
+
+    // Initialize the first row separately
+    let firstRow: GridNode[] = [];
+    for (let c = 0; c < COLUMNS; c++) {
+      firstRow.push(new GridNode(0));
+      if (c > 0) {
+        firstRow[c].left = firstRow[c - 1];
+        firstRow[c - 1].right = firstRow[c];
+      }
+    }
+    grid.push(firstRow);
+
+    // Initialize the rest of the grid
+    for (let r = 1; r < ROWS; r++) {
+      let row: GridNode[] = [];
+      for (let c = 0; c < COLUMNS; c++) {
+        row.push(new GridNode(0));
+        if (c > 0) {
+          row[c].left = row[c - 1];
+          row[c - 1].right = row[c];
+          row[c].topLeft = grid[r - 1][c - 1];
+          grid[r - 1][c - 1].bottomRight = row[c];
+        }
+        if (c < COLUMNS - 1) {
+          row[c].topRight = grid[r - 1][c + 1];
+          grid[r - 1][c + 1].bottomLeft = row[c];
+        }
+        row[c].top = grid[r - 1][c];
+        grid[r - 1][c].bottom = row[c];
+      }
+      grid.push(row);
+    }
+
+    return grid;
+  }
+
+  play(column: number): string {
+    if (this.gameFinished) {
       return "Game has finished!";
     }
 
-    for (let row = 5; row >= 0; row--) {
-      if (this.grid[row][col] === 0) {
-        // Empty cell found, place the player's token
-        this.grid[row][col] = this.currentPlayer;
-
-        if (this.checkWin(row, col)) {
-          // The current player wins the game
-          this.winner = this.currentPlayer;
-          return `Player ${this.currentPlayer} wins!`;
-        } else {
-          // Switch to the next player's turn
-          const message = `Player ${this.currentPlayer} has a turn`;
-          this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
-          return message;
-        }
-      }
+    if (column < 0 || column >= this.grid[0].length) {
+      return "Invalid column!";
     }
 
-    // No empty cell found in the specified column
+    let currentRow = this.grid.length - 1;
+    while (currentRow >= 0) {
+      if (this.grid[currentRow][column].value === 0) {
+        this.grid[currentRow][column].value = this.currentPlayer;
+
+        if (this.checkWin(currentRow, column)) {
+          this.gameFinished = true;
+          return "Player " + this.currentPlayer + " wins!";
+        }
+
+        this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+        return "Player " + this.currentPlayer + " has a turn";
+      }
+
+      currentRow--;
+    }
+
     return "Column full!";
   }
 
-  /**
-   * Checks if the current player has won the game.
-   * @param row - The row number of the last placed token.
-   * @param col - The column number of the last placed token.
-   * @returns True if the current player has won, false otherwise.
-   */
-  private checkWin(row: number, col: number): boolean {
-    // Check vertical
-    let count = 0;
-    for (let i = 0; i < 6; i++) {
-      count = this.grid[i][col] === this.currentPlayer ? count + 1 : 0;
-      if (count >= 4) return true;
+  checkWin(row: number, column: number): boolean {
+    const player = this.grid[row][column].value;
+
+    // Horizontal check
+    let count = 1;
+    let i = column - 1;
+    while (i >= 0 && this.grid[row][i].value === player) {
+      count++;
+      i--;
+    }
+    i = column + 1;
+    while (i < this.grid[row].length && this.grid[row][i].value === player) {
+      count++;
+      i++;
+    }
+    if (count >= 4) {
+      return true;
     }
 
-    // Check horizontal
-    count = 0;
-    for (let i = 0; i < 7; i++) {
-      count = this.grid[row][i] === this.currentPlayer ? count + 1 : 0;
-      if (count >= 4) return true;
+    // Vertical check
+    count = 1;
+    let j = row - 1;
+    while (j >= 0 && this.grid[j][column].value === player) {
+      count++;
+      j--;
+    }
+    j = row + 1;
+    while (j < this.grid.length && this.grid[j][column].value === player) {
+      count++;
+      j++;
+    }
+    if (count >= 4) {
+      return true;
+    }
+
+    // Diagonal check: top-left to bottom-right
+    count = 1;
+    i = column - 1;
+    j = row - 1;
+    while (i >= 0 && j >= 0 && this.grid[j][i].value === player) {
+      count++;
+      i--;
+      j--;
+    }
+    i = column + 1;
+    j = row + 1;
+    while (
+      i < this.grid[row].length &&
+      j < this.grid.length &&
+      this.grid[j][i].value === player
+    ) {
+      count++;
+      i++;
+      j++;
+    }
+    if (count >= 4) {
+      return true;
+    }
+
+    // Diagonal check: top-right to bottom-left
+    count = 1;
+    i = column + 1;
+    j = row - 1;
+    while (
+      i < this.grid[row].length &&
+      j >= 0 &&
+      this.grid[j][i].value === player
+    ) {
+      count++;
+      i++;
+      j--;
+    }
+    i = column - 1;
+    j = row + 1;
+    while (i >= 0 && j < this.grid.length && this.grid[j][i].value === player) {
+      count++;
+      i--;
+      j++;
+    }
+    if (count >= 4) {
+      return true;
     }
 
     return false;
   }
 }
-
-// Testing a vertical win
-const gameExample: Connect4 = new Connect4();
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0));
-console.log(gameExample.play(1));
-console.log(gameExample.play(0)); // Expected Result is Player 1 wins
-
-// Testing when a column is full
-const gameExample2: Connect4 = new Connect4();
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0));
-console.log(gameExample2.play(0)); // Expected Column Full
-console.log(gameExample2.play(0)); // Expected Column Full
-
-// Testing a horizontal win
-
-const gameExample3: Connect4 = new Connect4();
-console.log(gameExample3.play(0));
-console.log(gameExample3.play(0));
-console.log(gameExample3.play(1));
-console.log(gameExample3.play(1));
-console.log(gameExample3.play(2));
-console.log(gameExample3.play(2));
-console.log(gameExample3.play(3)); // Expected Player 1 Wins
-console.log(gameExample3.play(3)); // Expected Game Finished
